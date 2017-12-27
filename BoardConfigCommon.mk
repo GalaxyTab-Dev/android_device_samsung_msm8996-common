a78092b2eb7154322f84e65bb3b5ac90ef728c20
Alexander Gemsa <alexander.gemsa@gmail.com>
2017-12-27 20:51:48 +0100

    Split MKBOOTIMG Device Trees

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -55,7 +55,6 @@ BOARD_KERNEL_CMDLINE := console=null androidboot.hardware=qcom user_debug=31 msm
 BOARD_KERNEL_IMAGE_NAME := Image.gz
 BOARD_KERNEL_PAGESIZE := 4096
 BOARD_KERNEL_SEPARATED_DT := true
-BOARD_MKBOOTIMG_ARGS := --kernel_offset 0x00008000 --ramdisk_offset 0x02200000 --tags_offset 0x02000000 --board SRPPK08B000KU
 BOARD_CUSTOM_BOOTIMG_MK := hardware/samsung/mkbootimg.mk
 TARGET_KERNEL_SOURCE := kernel/samsung/gts3l
 
