82cab933a81e629a44b267474474d8ff7e4898b1
Alexander Gemsa <alexander.gemsa@gmail.com>
2017-12-31 12:56:58 +0100

    Clean Kernel Config

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -57,19 +57,9 @@ BOARD_KERNEL_PAGESIZE := 4096
 BOARD_KERNEL_SEPARATED_DT := true
 BOARD_CUSTOM_BOOTIMG_MK := hardware/samsung/mkbootimg.mk
 TARGET_KERNEL_SOURCE := kernel/samsung/gts3l
-
-# Kernel
-#BOARD_KERNEL_CMDLINE := console=null androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x37 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 cma=24M@0-0xffffffff rcupdate.rcu_expedited=1 androidboot.selinux=permissive
-#BOARD_KERNEL_SEPARATED_DT := true
-#BOARD_KERNEL_PAGESIZE := 4096
-#BOARD_KERNEL_BASE := 0x80000000
-#BOARD_MKBOOTIMG_ARGS := --kernel_offset 0x00008000 --ramdisk_offset 0x02200000 --tags_offset 0x02000000 --board SRPPK08B000KU
 TARGET_KERNEL_ARCH := arm64
 TARGET_KERNEL_HEADER_ARCH := arm64
-#TARGET_KERNEL_CONFIG := lineage_gts3lwifi_defconfig
-#TARGET_KERNEL_SOURCE := kernel/samsung/gts3l
 TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-linux-android-
-##BOARD_CUSTOM_BOOTIMG_MK := hardware/samsung/mkbootimg.mk
 
 # ANT+
 BOARD_ANT_WIRELESS_DEVICE := "vfs-prerelease"
